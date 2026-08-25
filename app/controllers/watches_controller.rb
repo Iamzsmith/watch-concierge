976fb51e28aa857836bf77b6b180427d085cd237
Zach Smith <46833746+Iamzsmith@users.noreply.github.com>
2026-08-24 21:11:04 -0400

    Add in a redirect for refresh and cancel submission

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,10 +1,11 @@
 class WatchesController < ApplicationController
   def index
+    @result = flash[:result]
   end
 
   def create
     @query = params[:query]
-    @result = WatchLookupService.new(@query).call
-    render :index
+    result = WatchLookupService.new(@query).call
+    redirect_to root_path(query: @query), flash: { result: result }
   end
 end
